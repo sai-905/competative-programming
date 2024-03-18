cace2f1f9a8c207c515428d516c46a4d44492485
sai-905 <163829330+sai-905@users.noreply.github.com>
2024-03-18 14:32:46 +0530

    Create largest possible sum of two integers without common digits.py

---
+++ largest possible sum of two integers without common digits.py
@@ -0,0 +1,17 @@
+lst = [15,0,105]
+l = len(lst)
+i = 0
+m = -1
+while i < 1 - 1:
+  j = i + 1
+  while j < l:
+    s = set(str(lst[i])))
+    t = set(str((lst[j]))
+    if not len(s.intersection(t)):
+            tmp = lst[i] + lst[j]
+            m = max(m, tmp)
+    j+=1
+  i+=1
+print(m)
+
+    
